24ac9ab3d6db80e8bba7e6d86c22d6048f916d30
matthijsvandermaas <matthijsvandermaas@yahoo.com>
2026-05-19 19:02:32 +0200

    Product klasse toegevoegd

--- backend/src/main/java/com/example/SpringBooth/POS/ProductController.java
+++ backend/src/main/java/com/example/SpringBooth/POS/ProductController.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 @RestController
 public class ProductController {
+
 	@GetMapping("/products")
-	public List<String> getProducts() {
-		return List.of("Pizza", "Cola", "Bier");
+	public List<Product> getProducts() {
+		return List.of(
+				new Product(1, "Pizza", 8.50, "Eten"),
+				new Product(2, "Cola", 2.50, "Drinken"),
+				new Product(3, "Bier", 3.00, "Drinken"));
 	}
 }
